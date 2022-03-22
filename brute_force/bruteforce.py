f2ae71a4852ec09e95d3cb3288d88cc65d45f56c
Julien Garcia <julien.garcia-lp@laposte.net>
2022-03-22 16:56:06 +0100

    Remplacement boucle for par boucle while

--- brute_force/bruteforce.py
+++ brute_force/bruteforce.py
@@ -4,7 +4,9 @@ from itertools import combinations
 from actions_class import Action
 from utils import execution_time
 
+
 BUDGET = 500
+
 
 with open('../data_csv/first_list.csv', 'r') as f:
     actions_file = csv.reader(f)
@@ -15,13 +17,15 @@ with open('../data_csv/first_list.csv', 'r') as f:
 def bruteforce(actions):
     max_profit = 0
     best_combination = None
-    for n in range(2, len(actions)):
+    n = 1
+    while n <= len(actions):
         for comb in combinations(actions, n):
             money_spent = sum([action_price.cost for action_price in comb])
             total_profit = sum([profit.profit for profit in comb])
             if total_profit > max_profit and money_spent <= BUDGET:
                 max_profit = total_profit
                 best_combination = comb
+        n += 1
     return best_combination
 
 
