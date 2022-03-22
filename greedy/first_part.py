31e6a51d73b4efa57960200984425925b5a13389
Julien Garcia <julien.garcia-lp@laposte.net>
2022-03-22 16:03:02 +0100

    Implémentation de la classe Action

--- greedy/first_part.py
+++ greedy/first_part.py
@@ -1,43 +1,41 @@
 import csv
-import time
+from typing import Tuple, List
 
+from actions_class import Action
 from utils import execution_time
-
 
 BUDGET = 500
 
-with open('../csv/first_list.csv', 'r') as f:
+with open('../data_csv/first_list.csv', 'r') as f:
     actions_file = csv.reader(f)
-    all_actions = []
-
-    for action in actions_file:
-        name = action[0]
-        cost = int(action[1])
-        profit = round(cost * (1 + int(action[2][:-1])/100), 2)
-
-        # Action format: (Name, Price, Profit after 2 years)
-        all_actions.append((name, cost, profit))
-# print(*all_actions, sep="\n")
+    all_actions = [Action(action) for action in actions_file]
 
 
 @execution_time
-def greedy(budget, actions):
-    sorted_actions = sorted(actions, key=lambda x: x[2])
+def greedy(budget: int, actions) -> Tuple[List[Action], float, float]:
+    """
+    Greedy algorithm to quickly calculate an estimate of the most profitable stocks
+    :param budget: budget limit not to be exceeded
+    :param actions: list of all actions
+    :return: (selected_actions, total actions price, total gain after 2 years)
+    """
+    sorted_actions = sorted(actions, key=lambda x: x.profit)
     selected_actions = []
     money_spent = 0
 
     while sorted_actions:
         current_action = sorted_actions.pop()
-        if current_action[1] + money_spent <= budget:
+        if current_action.cost + money_spent <= budget:
             selected_actions.append(current_action)
-            money_spent += current_action[1]
+            money_spent += current_action.cost
 
-    return [action[0] for action in selected_actions], money_spent, sum([action[2] for action in selected_actions])
+    return ([action.name for action in selected_actions],
+            money_spent,
+            sum([action.profit for action in selected_actions]))
 
 
 best_selected_actions = greedy(BUDGET, all_actions)
 
 print(f"Actions sélectionnées: {best_selected_actions[0]}")
-print(f"Argent dépensé: {best_selected_actions[1]}€.")
-print(f"Profit: {best_selected_actions[2]}€.")
-
+print(f"Argent dépensé: {best_selected_actions[1]:.2f}€.")
+print(f"Profit: {best_selected_actions[2]:.2f}€.")
