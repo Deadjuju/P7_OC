bed0367647da44d271b22db2e2942dca62e08dc1
Julien Garcia <julien.garcia-lp@laposte.net>
2022-03-23 15:33:54 +0100

    modification de extract csv pour intégrer le cas des nombres décimaux pour optimized

--- utils.py
+++ utils.py
@@ -12,10 +12,26 @@ def execution_time(funct):
         print(f"EXECUTION: {(end_time - start_time):.3f}s\n")
 
         return funct(*args, **kwargs)
+
     return wrapper
 
 
-def extract_csv(path):
-    with open(path, 'r') as f:
-        actions_file = csv.reader(f)
-        return [Action(action) for action in actions_file if action != ['name', 'price', 'profit']]
+def extract_csv(path, is_decimal_prices=False):
+    if not is_decimal_prices:
+        with open(path, 'r') as f:
+            actions_file = csv.reader(f)
+            return [Action(action) for action in actions_file if action != ['name', 'price', 'profit']]
+
+    else:
+        with open(path, 'r') as f:
+            actions_file = csv.reader(f)
+            all_actions = []
+
+            for action in actions_file:
+                if action != ['name', 'price', 'profit']:
+                    name = action[0]
+                    cost = float(action[1])*100
+                    profit = round(float(action[2][:-1]) / 100, 2)*100
+                    if cost > 0:
+                        all_actions.append(Action([name, str(cost), str(profit)]))
+            return all_actions
