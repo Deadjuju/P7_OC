db0115b0082cec7f94483326b73ab9381a9fac7c
Julien Garcia <julien.garcia-lp@laposte.net>
2022-03-30 15:59:51 +0200

    Correction noms de variables

--- bruteforce_algo/bruteforce.py
+++ bruteforce_algo/bruteforce.py
@@ -18,8 +18,8 @@ def brute_force(budget: int, stocks: list) -> List[Stock]:
     n = 1
     while n <= len(stocks):
         for comb in combinations(stocks, n):
-            money_spent = sum([action_price.cost for action_price in comb])
-            total_profit = sum([profit.profit for profit in comb])
+            money_spent = sum([stock.cost for stock in comb])
+            total_profit = sum([stock.profit for stock in comb])
             if total_profit > max_profit and money_spent <= budget:
                 max_profit = total_profit
                 best_combination = comb
@@ -31,13 +31,12 @@ if __name__ == '__main__':
     from utils import extract_csv
 
     file = '../data_csv/first_list.csv'
-
     BUDGET = 500
 
-    all_actions = extract_csv(path=file)
-    best_comb = brute_force(budget=BUDGET, stocks=all_actions)
+    all_stocks = extract_csv(path=file)
+    best_comb = brute_force(budget=BUDGET, stocks=all_stocks)
 
     print("-------------- Best Choice: --------------")
     print(best_comb)
-    print(f"Argent dépensé: {sum([action_price.cost for action_price in best_comb])}€.")
+    print(f"Argent dépensé: {sum([price.cost for price in best_comb])}€.")
     print(f"Profit: {(sum([profit.profit for profit in best_comb])):.2f}€.")
